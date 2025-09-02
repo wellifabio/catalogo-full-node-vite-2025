4084c705fe3713c26fd2ba4d6a09d87e422ce4bb
wellifabio <wellifabio@hotmail.com>
2025-09-02 16:37:56 -0300

    iniciado html

---
+++ web/plantas/src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  return(
+    <>
+    <header>
+      <h1>Catálogo de Plantas</h1>
+    </header>
+    <main>
+      <h2>Bem-vindo ao nosso catálogo de plantas!</h2>
+    </main>
+    <footer>
+      <p>By wellifabio</p>
+    </footer>
+    </>
+  )
+}
+
+export default App;
