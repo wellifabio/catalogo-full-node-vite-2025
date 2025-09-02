73e6973ad8a4d9dc9a9a4e374a1a6c4645efda35
wellifabio <wellifabio@hotmail.com>
2025-09-02 15:59:13 -0300

    rota put alterada para patch

--- api/src/routes.js
+++ api/src/routes.js
@@ -8,7 +8,7 @@ routes.get("/",(req, res)=>{
             { method: "GET", path: "/plantas", description: "Listar todas as plantas" },
             { method: "GET", path: "/plantas/:id", description: "Buscar planta por ID" },
             { method: "POST", path: "/plantas", description: "Criar nova planta" },
-            { method: "PUT", path: "/plantas/:id", description: "Atualizar planta por ID" },
+            { method: "PATCH", path: "/plantas/:id", description: "Atualizar planta por ID" },
             { method: "DELETE", path: "/plantas/:id", description: "Deletar planta por ID" }
         ]
     });
@@ -19,7 +19,7 @@ const Planta = require("./controllers/planta");
 routes.get("/plantas", Planta.readAll);
 routes.get("/plantas/:id", Planta.readOne);
 routes.post("/plantas", Planta.create);
-routes.put("/plantas/:id", Planta.update);
+routes.patch("/plantas/:id", Planta.update);
 routes.delete("/plantas/:id", Planta.del);
 
 module.exports = routes;
