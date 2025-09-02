125fd4f386b4f9a940528fcd7cfdb189ee7869ba
wellifabio <wellifabio@hotmail.com>
2025-09-02 15:20:16 -0300

    server atualizado

--- api/api/server.js
+++ api/api/server.js
@@ -3,9 +3,11 @@ const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3001
-
+const routes = require("../src/routes");
+    
 app.use(cors());
 app.use(express.json());
+app.use("/", routes);
 
 app.listen(port, () => {
     console.log(`API respondendo em http://localhost:${port}`);
