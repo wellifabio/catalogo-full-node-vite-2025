37dfa67bb129c85492a80394a27f3a9fa0c337dc
wellifabio <wellifabio@hotmail.com>
2025-09-02 16:45:51 -0300

    app.js listando as plantas

--- web/plantas/src/App.jsx
+++ web/plantas/src/App.jsx
@@ -1,14 +1,34 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [plantas, setPlantas] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'http://localhost:3001/plantas',
+      );
+      setPlantas(result.data);
+    };
+    fetchData();
+  }, []);
+
   return(
     <>
     <header>
       <h1>Catálogo de Plantas</h1>
     </header>
     <main>
-      <h2>Bem-vindo ao nosso catálogo de plantas!</h2>
+      {
+        plantas.map( planta => (
+          <div key={planta.id} className="card">
+            <h2>{planta.nome}</h2>
+            <p>{planta.descricao}</p>
+          </div>
+        ))
+      }
     </main>
     <footer>
       <p>By wellifabio</p>
